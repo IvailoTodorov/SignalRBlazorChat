6ced2cefc86a3bc6e09162d34e10e016b331f7cd
IvayloTodorov <ivo_todorov.ab@abv.bg>
2022-07-06 14:02:49 +0300

    Add project files.

---
+++ SignalRBlazorChat/Server/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿namespace SignalRBlazorChat.Server.Hubs
+{
+    using Microsoft.AspNetCore.SignalR;
+    using System;
+    using System.Threading.Tasks;
+
+    public class ChatHub : Hub
+    {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
+        public override async Task OnConnectedAsync()
+        {
+            var username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await SendMessage(string.Empty, $"{username} connected!");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            var username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await SendMessage(string.Empty, $"{username} left!");
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("GotchaMessage", user, message);
+        }
+    }
+}
